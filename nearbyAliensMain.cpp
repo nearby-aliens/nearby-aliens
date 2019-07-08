b62d90921b27cfe56ea9b0e704e7445a1ac7bbd8
Jessica Barnett <jessba2@pdx.edu>
2019-07-08 12:10:05 -0700

    began game with opening

---
+++ nearbyAliensMain.cpp
@@ -0,0 +1,17 @@
+//Jessica Barnett
+//Summer 2019
+//nearbyAliensMain.cpp
+
+
+
+#include "nearbyAliensMain.h"
+using namespace std;
+int main()
+{
+
+cout << "Welcome to Nearby Aliens. You will search nearby star systems for inteligent life." <<endl;
+
+
+return 0;
+}
+
---
+++ nearbyAliensMain.h
@@ -0,0 +1,11 @@
+//Jessica Barnett
+//Summer 2019
+//nearbyAliensMain.cpp
+
+
+#include <cctype>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+
+
