981a9576f48f00cc5ca0866f640e882ba285dd9d
Jessica Barnett <jessba2@pdx.edu>
2019-07-08 14:01:22 -0700

    added io for first user choice

--- nearbyAliensMain.cpp
+++ nearbyAliensMain.cpp
@@ -1,17 +1,30 @@
-//Jessica Barnett
+//Jessica Barnett and Thomas Honnell
 //Summer 2019
 //nearbyAliensMain.cpp
 
-
-
-#include "nearbyAliensMain.h"
 using namespace std;
+
+#include <cctype>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+
 int main()
 {
+				int starChoice;
 
-cout << "Welcome to Nearby Aliens. You will search nearby star systems for inteligent life." <<endl;
-
-
-return 0;
+				cout << "Welcome to Nearby Aliens. You will search nearby star systems for inteligent life." <<endl <<endl;
+				cout << "Begin your journey by choosing a star system." <<endl;
+				cout << "	1)Proxima Centauri (4.24 light years)"<<endl;
+				cout << " 2)Alpha Centauri binary pair (4.37 light years)"<<endl;
+				cout << " 3)Bernard's Star (5.96 light years)" <<endl;
+				cout << " 4)Wolf 359 (7.78 light years)" <<endl;
+				cout << " 5)Ross 128 (11.03 light years)" <<endl;
+				cout << "Enter the number of your choice: " <<endl;
+				cin >> starChoice;
+				cin.ignore(30, '\n');
+				cout << "You have chosen " << starChoice << ". Good Choice.  Please enter the cryo-chamber, and I will wake you when we arrive." <<endl;
+				return 0;
 }
 
--- nearbyAliensMain.h
+++ nearbyAliensMain.h
@@ -2,10 +2,10 @@
 //Summer 2019
 //nearbyAliensMain.cpp
 
-
+/*
 #include <cctype>
 #include <cstring>
 #include <cstdlib>
 #include <iostream>
-
+*/
 
