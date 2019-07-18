120af082c85f4e0affd709d0a07e63115fe5f516
Jessica Barnett <jessba2@pdx.edu>
2019-07-18 08:54:50 -0700

    updated display

--- nearbyAliensMain.cpp
+++ nearbyAliensMain.cpp
@@ -9,18 +9,18 @@ int main()
 {
   int starChoice = 99;
 
-  cout << endl << endl << "Welcome to Nearby Aliens. You will search nearby star systems for inteligent life." <<endl <<endl;
-  cout << "Begin your journey by choosing a star system." <<endl;
+  cout << endl << endl << BLUE <<"Welcome to Nearby Aliens. You will search nearby star systems for inteligent life." <<endl <<endl;
+  cout << MAGENTA << "Begin your journey by choosing a star system." <<endl;
   while(starChoice !=2) //(starChoice<=0 || starChoice >=6)
   {
 cout <<endl;
-    cout << " Please enter your choice from 1 to 5:  (currently only choice 2 is implemented, choose 2!" <<endl <<endl;
-    cout << "   1)Proxima Centauri (4.24 light years)" <<endl;
+    cout <<BLUE<< " Please enter your choice from 1 to 5:  (currently only choice 2 is implemented, choose 2!" <<endl <<endl;
+    cout <<CYAN<< "   1)Proxima Centauri (4.24 light years)" <<endl;
     cout << "   2)Alpha Centauri binary pair (4.37 light years)"<<endl;
     cout << "   3)Bernard's Star (5.96 light years)" <<endl;
     cout << "   4)Wolf 359 (7.78 light years)" <<endl;
     cout << "   5)Ross 128 (11.03 light years)" <<endl;
-    cout << " Enter the number of your choice: " <<endl;
+    cout <<BLUE<< " Enter the number of your choice: " << RESET <<endl;
     cin >> starChoice;
     cin.ignore(30, '\n');
     if(starChoice !=2)
@@ -46,15 +46,6 @@ cout <<endl;
   aStarSystem.printStarSystem();
   cout <<endl;
 
-  cout << "Practice display" << endl;
-  cout << RED << "        w for up" <<endl;
-  cout << GREEN << "a for left        d for right" <<endl;
-  cout << YELLOW << "       s for down" <<endl;
-  cout << CYAN << "*********************************************************************************************************************************************" << endl;
-  cout << BLUE << "This is blue" << endl;
-  cout << MAGENTA << "This is magenta" << endl << endl << endl;
-
-  cout << RESET << "Bottom of practice display" <<endl;
   return 0;
 
 }
